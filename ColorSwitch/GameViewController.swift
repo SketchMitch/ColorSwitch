4a36f1aa978c0e5cc9eee2f3b5c7f64878b4a23e
SketchMitch <sketch0306@gmail.com>
2020-05-11 17:55:21 -0400

    added the rest of the files

---
+++ ColorSwitch/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  ColorSwitch
+//
+//  Created by Mitch on 4/29/20.
+//  Copyright © 2020 Mitchell Developer. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene = MenuScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
